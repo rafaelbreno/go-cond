7c2a3a5d70995220442270cf39302e5f2dc02b4a
Rafael Breno <rafael_breno@outlook.com>
2021-01-05 20:03:39 -0300

    Add 'and' condition

--- cond/cond.go
+++ cond/cond.go
@@ -1,11 +1,32 @@
 package cond
 
 type Condition struct {
-	Vars []bool
+	CurrentVal bool
+}
+
+type Wrapper struct {
+	Value interface{}
 }
 
 func NewCondition() Condition {
 	return Condition{}
 }
 
-//func (c *Condition) CondOR ()
+func (c *Condition) And(conds ...bool) *Condition {
+	l := len(conds)
+
+	c.CurrentVal = conds[0] && conds[1]
+
+	if l == 2 {
+		c.CurrentVal = conds[0] && conds[1]
+	}
+
+	for i := 2; i < l; i++ {
+		c.CurrentVal = c.CurrentVal && conds[i]
+	}
+
+	return c
+}
+
+// Go from https://play.golang.org/p/WOnE31ldpb
+// Lines: 70~81
--- cond/cond_test.go
+++ cond/cond_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func testNewCondition(t *testing.T) {
+func TestNewCondition(t *testing.T) {
 	t.Helper()
 
 	want := cond.Condition{}
@@ -15,4 +15,18 @@ func testNewCondition(t *testing.T) {
 		t.Errorf("Want %v\nGot %v", want, got)
 	}
 
+}
+
+func TestAnd(t *testing.T) {
+	t.Helper()
+
+	want := false
+
+	c := cond.NewCondition()
+
+	got := c.And(true, false, false, false, true).CurrentVal
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Want %v\nGot %v", want, got)
+	}
 }
